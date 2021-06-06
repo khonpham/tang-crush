8ebf2382466d916cc3306d28e1165ec5fc57344b
khonpham <85432546+khonpham@users.noreply.github.com>
2021-06-06 20:34:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     introTitle: 'Tiểu Quyên à!',
-    introDesc: `Lớp học vốn rất nhiều người
-    Cớ sao chỉ thấy nụ cười của em 
-    Ngôi sao lấp lánh giữa đêm
+    introDesc: `Lớp học vốn rất nhiều người.
+    Cớ sao chỉ thấy nụ cười của em. 
+    Ngôi sao lấp lánh giữa đêm.
     Tim anh bổng thấy êm đềm lạ thay `,
-    btnIntro: 'hi',
+    btnIntro: '❤',
     title: 'Phải chăng em đã yêu anh từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy anh đẹp trai này ',
-    btnYes: 'Làm vợ anh nhá <33',
-    btnNo: 'Không, sau này tính :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
-    btnReply: 'Gửi cho em <3',
+    btnYes: 'Làm vợ anh nhá ❤',
+    btnNo: 'Không, sau này tính!',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 10h anh về chơi bi-da nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'Anh biết mà. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 10h anh về chơi bi-da nhé công túa.',
+    btnAccept: 'Vậy nhá bé con <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
